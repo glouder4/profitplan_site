7d4223bfd72afc1eaa603af6977be0db7d1a7021
glouder4 <egorov.2001@bk.ru>
2023-06-16 19:40:43 +0500

    Добавлен блок "Портфолио"

--- wp-content/themes/profitplan_theme/base_templates/frontpage.php
+++ wp-content/themes/profitplan_theme/base_templates/frontpage.php
@@ -91,14 +91,203 @@ get_header();
         </div>
     </div>
 </section>
-<section id="steps_of_work">
+<section id="steps_of_work" class="mt-3">
+    <div class="section_title pt-4 pb-4 bg_fixed" style="background-image: url('<?=get_template_directory_uri();?>/base_assets/steps_of_work.jpg');">
+        <div class="container">
+            <h3 class="text-white bg_title">Основные этапы разработки и внедрения систем управления</h3>
+        </div>
+    </div>
     <div class="container">
-        <h3>Основные этапы разработки и внедрения систем управления</h3>
+        <div class="row ms-0 me-0" id="steps">
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>01. Автоматизируем процессы работы</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>02. Настроим сквозную аналитику</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>03. Интегрируем систему с кассой</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>04. Оцифруем бизнес процессы</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>05. Отобразим положение дел компании в инфографике</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>06. Проконтролируем склад предприятия</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>07. Рассчитаем ЗП сотрудникам</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>08. Отобразим ключевые показатели эффективности предприятия</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+
+            <div class="col-12 step_item mt-2">
+                <div class="step_item-title">
+                    <h4>09. Проконтролируем действия сотрудников</h4>
+                </div>
+                <div class="step_item-desc">
+                    <p>Очень увлекательный текст</p>
+                </div>
+
+                <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <rect fill="none" id="canvas_background" height="402" width="582" y="-1" x="-1"/>
+                    </g>
+                    <g>
+                        <path transform="rotate(-90 13.000000000000002,13) " id="svg_1" fill="#27273c" d="m0,0l0,13l13,0l0,13l13,0l0,-26l-26,0z"/>
+                    </g>
+                </svg>
+            </div>
+        </div>
     </div>
 </section>
-<section id="we_are_best">
+<section id="we_are_best" class="mt-3">
     <div class="container">
-
+        <div class="section_title">
+            <h4>Наш опыт в разработке и во внедрении систем управления предприятием</h4>
+        </div>
+        <div class="section_description">
+            <p>Небольшой абзац про то, какие мы хорошие</p>
+        </div>
+        <div id="portfolio_section">
+            <div class="row ms-0 me-0">
+                <div class="col-12 p-0">
+                    <div class="col-md-4">
+                        <div class="card portfolio_item" style="width: 18rem;">
+                            <img src="https://i.imgur.com/ZTkt4I5.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Название проекта</h5>
+                                <p class="card-text">Крактое описание проекта</p>
+                                <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Подробнее </a>
+                                <a href="#" class="btn "><i class="fab fa-github"></i> Демо</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <?php
